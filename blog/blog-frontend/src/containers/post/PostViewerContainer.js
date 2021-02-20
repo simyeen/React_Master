5eed4962484730efc832d1b98abb16d118346ca9
simyeen <simyeen@naver.com>
2021-02-20 16:08:17 +0900

    feat/updatePosts api

--- blog/blog-frontend/src/containers/post/PostViewerContainer.js
+++ blog/blog-frontend/src/containers/post/PostViewerContainer.js
@@ -4,15 +4,21 @@ import { withRouter } from 'react-router-dom';
 import PostActionButton from '../../components/post/PostActionButton';
 import PostViewer2 from '../../components/post/PostViewer2';
 import { readPost, unloadPost } from '../../modules/post';
+import { setOriginalPost } from '../../modules/write';
 
-const PostViewerContainer = ({ match }) => {
+const PostViewerContainer = ({ match, history }) => {
   const { postId } = match.params;
   const dispatch = useDispatch();
-  const { post, error, loading } = useSelector(({ post, loading }) => ({
-    post: post.post,
-    error: post.error,
-    loading: loading['post/READ_POST'],
-  }));
+  const { post, error, loading, user } = useSelector(
+    ({ post, loading, user }) => ({
+      post: post.post,
+      error: post.error,
+
+      loading: loading['post/READ_POST'],
+
+      user: user.user,
+    }),
+  );
 
   useEffect(() => {
     //처음 마운트될 때 포스트 읽기 API 요청.
@@ -24,13 +30,20 @@ const PostViewerContainer = ({ match }) => {
     };
   }, [dispatch, postId]);
 
+  const onEdit = () => {
+    dispatch(setOriginalPost(post));
+    history.push('/write');
+  };
+
+  const ownPost = (user && user._id) === (post && post.user._id);
+
   return (
     <>
       <PostViewer2
         post={post}
         error={error}
         loading={loading}
-        actionButtons={<PostActionButton />}
+        actionButtons={ownPost && <PostActionButton onEdit={onEdit} />}
       ></PostViewer2>
     </>
   );
